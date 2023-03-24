5a8c526ef2f68f3d37a56a310f49199b05599b42
saurav-commits <sauravsky55@yahoo.com>
2023-03-24 15:48:55 +0530

    Register Login Logout

---
+++ server.js
@@ -0,0 +1,98 @@
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+    }
+
+
+const express = require("express");
+const app = express()
+const bcrypt = require("bcrypt")
+const initializePassport = require("./passport-config")
+const flash = require("express-flash")
+const session = require("express-session");
+const passport = require("passport");
+const dotenv = require("dotenv").config();
+const methodOverride = require("method-override");
+
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+
+const users = []
+
+
+app.use(express.urlencoded({extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, // we wont resave the session if nothing has changed
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride("_method"))
+
+// configuring the login post functionality
+app.post("/login", checkNotAuthenticated, passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true 
+}))
+
+
+// configuring the register post functionality
+app.post("/register",checkNotAuthenticated, async(req, res)=> {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+        })
+        console.log(users) // display the users logged in
+        res.redirect('/login')
+    } catch(error) {
+        console.log(error);
+        res.redirect("/register")
+    }
+})
+
+app.get('/', checkAuthenticated, (req, res)=> {
+    res.render("index.ejs", {name: req.user.name})
+})
+
+ 
+app.get('/login', checkNotAuthenticated, (req, res)=> {
+    res.render("login.ejs")
+})
+
+app.get('/register' , checkNotAuthenticated, (req, res) => {
+    res.render("register.ejs")
+})
+
+app.delete("/logout", (req, res) => {
+    req.logOut(req.user, err => {
+        if(err) return next(err)
+        res.redirect("/")
+    })
+})
+
+
+function checkAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+        return next()
+    }
+    res.redirect("/login")
+}
+
+function checkNotAuthenticated(req, res, next) {
+    if(req.isAuthenticated()) {
+       return res.redirect("/login")
+    }
+    next()
+}
+
+app.listen(3000);
